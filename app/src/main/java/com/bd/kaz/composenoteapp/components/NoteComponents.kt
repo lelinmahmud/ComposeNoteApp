a62f17d304f758fb710532aa03699107e586a5c1
Lelin Mahmud <lelinaits20@gmail.com>
2022-02-23 01:16:53 +0600

    save button added

--- app/src/main/java/com/bd/kaz/composenoteapp/components/NoteComponents.kt
+++ app/src/main/java/com/bd/kaz/composenoteapp/components/NoteComponents.kt
@@ -1,7 +1,9 @@
 package com.bd.kaz.composenoteapp.components
 
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
@@ -44,4 +46,22 @@ fun NoteInputText(
         modifier = modifier
     )
 
+}
+
+@Composable
+fun NoteButton(
+    modifier: Modifier = Modifier,
+    label: String,
+    onClick: ()-> Unit,
+    enabled: Boolean = true
+){
+    Button(
+        onClick = { onClick },
+        shape = CircleShape,
+        enabled = enabled,
+        modifier = modifier
+        ) {
+        
+        Text(text = label)
+    }
 }
--- app/src/main/java/com/bd/kaz/composenoteapp/screen/NoteScreen.kt
+++ app/src/main/java/com/bd/kaz/composenoteapp/screen/NoteScreen.kt
@@ -9,15 +9,27 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Notifications
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.bd.kaz.composenoteapp.components.NoteButton
 import com.bd.kaz.composenoteapp.components.NoteInputText
 
 @Composable
 fun NoteScreen(){
+
+    var title = remember {
+        mutableStateOf("")
+    }
+
+    var description = remember {
+        mutableStateOf("")
+    }
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -36,7 +48,24 @@ fun NoteScreen(){
             modifier = Modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            NoteInputText(text = "Hello", label = "Note", onTextChange ={} )
+            NoteInputText(
+                text = "${title.value}",
+                label = "Title",
+                onTextChange ={
+                     if (it.all { char->
+                             char.isLetter() || char.isWhitespace()
+                         }) title.value = it
+            }, modifier = Modifier.padding(top = 9.dp, bottom = 8.dp) )
+            NoteInputText(
+                text = "${description.value}",
+                label = "Add a Note",
+                onTextChange ={
+                    if (it.all { char->
+                            char.isLetter() || char.isWhitespace()
+                        }) description.value = it
+                },
+                modifier = Modifier.padding(top = 9.dp, bottom = 8.dp ))
+            NoteButton(label = "Save", onClick = {  })
         }
         
     }
